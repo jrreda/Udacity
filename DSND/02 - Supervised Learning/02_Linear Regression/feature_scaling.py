20dd1a1ff724a4ab94e455accb3b33d50fe480ea
jrreda <mahmoudreda457@gmail.com>
2020-06-09 15:55:28 +0200

    P2 Lessons

---
+++ DSND/02 - Supervised Learning/02_Linear Regression/feature_scaling.py
@@ -0,0 +1,24 @@
+# TODO: Add import statements
+
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = None
+X = None
+y = None
+
+# TODO: Create the standardization scaling object.
+scaler = None
+
+# TODO: Fit the standardization parameters and scale the data.
+X_scaled = None
+
+# TODO: Create the linear regression model with lasso regularization.
+lasso_reg = None
+
+# TODO: Fit the model.
+
+
+# TODO: Retrieve and print out the coefficients from the regression model.
+reg_coef = None
+print(reg_coef)
---
+++ DSND/02 - Supervised Learning/02_Linear Regression/regularization.py
@@ -0,0 +1,18 @@
+# TODO: Add import statements
+
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = None
+X = None
+y = None
+
+# TODO: Create the linear regression model with lasso regularization.
+lasso_reg = None
+
+# TODO: Fit the model.
+
+
+# TODO: Retrieve and print out the coefficients from the regression model.
+reg_coef = None
+print(reg_coef)
