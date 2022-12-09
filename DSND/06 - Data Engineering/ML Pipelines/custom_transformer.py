d3e3e1986e2e3d287086ae43a49e9df960117d78
Mahmoud Reda <mahmoudreda457@gmail.com>
2022-12-09 20:23:02 +0200

    Add ML Pipelines

---
+++ DSND/06 - Data Engineering/ML Pipelines/custom_transformer.py
@@ -0,0 +1,33 @@
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+import re
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+def tokenize(text):
+    text = re.sub(r'http\S+', 'url_placeholder', text.lower()).strip()
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    lemms = [lemmatizer.lemmatize(token) for token in tokens]
+    return lemms
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
